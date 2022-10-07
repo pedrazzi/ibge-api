bbc058f1e83d764c62ddc5ffabda051655ba7f38
Pedrazzi <pedrazzi@hotmail.com>
2022-10-07 18:49:57 -0400

    Update Request.php

--- src/Request.php
+++ src/Request.php
@@ -19,20 +19,62 @@ class Request
 	{
 	}
 
+	/**
+	 * Atualiza a instancia
+	 *
+	 * @param string $name
+	 * @param string $url
+	 * @author Fábio Pedrazzi <pedrazzi@hotmail.com>
+	 * @date 07/10/2022
+	 */
 	public function set(string $name, string $url): void
 	{
 		$this->name = $name;
 		$this->url = self::URL_BASE . "/$this->version/$url";
 	}
 
+	/**
+	 * Ordena requisição por nome
+	 *
+	 * @author Fábio Pedrazzi <pedrazzi@hotmail.com>
+	 * @date 07/10/2022
+	 */
 	public function setOrderName(): void
 	{
 		$this->module->params['orderBy'] = "name";
 	}
 
+	/**
+	 * Remove ordem por nome
+	 *
+	 * @author Fábio Pedrazzi <pedrazzi@hotmail.com>
+	 * @date 07/10/2022
+	 */
+	public function removeOrderName(): void
+	{
+		unset($this->module->params['orderBy']);
+	}
+
+	/**
+	 * Nivela hierarquia JSON da requisição
+	 *
+	 * @author Fábio Pedrazzi <pedrazzi@hotmail.com>
+	 * @date 07/10/2022
+	 */
 	public function setNivelado(): void
 	{
 		$this->module->params['view'] = "nivelado";
+	}
+
+	/**
+	 * Remove o nivelamento da hierarquia JSON da requisição
+	 *
+	 * @author Fábio Pedrazzi <pedrazzi@hotmail.com>
+	 * @date 07/10/2022
+	 */
+	public function removeNivelado(): void
+	{
+		unset($this->module->params['view']);
 	}
 
 	/**
